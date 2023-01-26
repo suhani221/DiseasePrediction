791c6bbd4a4ec54beeae4de1a7d1d652ad5334a2
suhani221 <suhani.agarwal@plaksha.edu.in>
2023-01-27 04:02:05 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,11 +1,11 @@
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-
+import joblib
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+diabetes_model = joblib.load("diabetes_model.sav")
 heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
